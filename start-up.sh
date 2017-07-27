419bc46125108f25c6bfdb046a6e9959030bda0b
Chareice <chareice@live.com>
2017-07-27 19:27:08 +0800

    ADD: docker compose

--- start-up.sh
+++ start-up.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-echo 'write environment variables..'
-export APP_DEBUG=false
-export APP_ENV=production
-export APP_KEY=SomeRandomKey!!!
-export DB_CONNECTION=mysql
-export DB_HOST=$MYSQL_PORT_3306_TCP_ADDR
-export DB_PORT=$MYSQL_PORT_3306_TCP_PORT
-export DB_DATABASE=message_server
-export DB_USERNAME=root
-export DB_PASSWORD=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
-export CACHE_DRIVER=memcached
-export QUEUE_DRIVER=sync
-export APP_TIMEZONE=PRC
-export DB_TIMEZONE=+08:00
-echo 'change to workdir'
 cd /var/www/webapp;
 echo "start migrate"
 php artisan migrate --force
